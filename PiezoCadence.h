004ebb89e681e781a696f193f69b29e1dce6619d
eborchardt <56270998+eborchardt@users.noreply.github.com>
2024-11-21 23:41:26 -0600

    Eborchardt patch 1 (#1)
    * Create PiezoCadence.h
    
    * Create PiezoCadence.cpp
    
    * Update PiezoCadence.h
    
    * Update README.md

---
+++ PiezoCadence.h
@@ -0,0 +1,22 @@
+#ifndef PiezoCadence_h
+#define PiezoCadence_h
+
+class PiezoCadence {
+  public:
+    PiezoCadence(int pin, int threshold = 400, int maSize = 5);
+    void begin();
+    int getCadence();
+  private:
+    void stepDetection();
+    int _pin;
+    int _threshold;
+    int _maSize;
+    volatile int count;
+    unsigned long lastTime;
+    unsigned long thisTime;
+    int inter;
+    int maValues[MA_SIZE];
+    int maIndex;
+};
+
+#endif
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # PiezoCadenceSensor
+Calculate the cadence as detected by a piezo sensor connected to an ESP32. Written as a library so i can use it in my Bluetooth treadmill project.
